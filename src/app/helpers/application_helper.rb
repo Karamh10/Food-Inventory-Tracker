809119b7c14e805ed944ee6380e252bf30827f5f
Student 1 Account <student1@hpc263.tcnj.edu>
2021-04-22 19:49:45 -0400

    Log out fix

---
+++ src/app/helpers/application_helper.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Helper functions made here are accessable in all views
+# for example, app_name is now accessable to all views
+module ApplicationHelper
+  def app_name
+    conf = app_config
+    if conf.nil?
+      '?'
+    else
+      conf.name
+    end
+  end
+
+  private
+
+  def app_config
+    AppConfig.first
+  end
+  
+  def nav_link(text, path)
+    if current_page?(path)
+        content_tag :li, class: "nav-items active" do
+            link_to(path, class: "nav-link") do
+                sanitize(text) + content_tag(:span, "(current)", class: "sr-only")
+    
+            end
+        end
+    else
+        content_tag :li, class: "nav-items" do
+            link_to(text, path, class: 'nav-link')
+        end
+    end
+  end    
+end
---
+++ src/app/helpers/home_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Helper methods accessable for views within the home folder
+module HomeHelper
+
+end
