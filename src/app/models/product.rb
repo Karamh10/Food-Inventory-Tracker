d0597d51b0fcf168f04dc1d4624eeef41858f8c5
Student 1 Account <student1@hpc263.tcnj.edu>
2021-05-12 16:39:09 -0400

    search bar implementation

--- src/app/models/product.rb
+++ src/app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
     belongs_to :user
+    #search bar
+    def self.search(search)
+        if search
+            where(["product LIKE ?", "%#{search}%"])
+        else
+            all
+        end
+    end
+
 end
